937de17ada0d6ebb17af790c06bb53907bb5fd9b
yammaru <63098405+yammaru@users.noreply.github.com>
2021-11-22 09:13:48 -0500

    lol
    looool

---
+++ Maiz/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace Maiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBoxVentilador = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSolLuna = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCultivo = new System.Windows.Forms.PictureBox();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVentilador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSolLuna)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCultivo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.progressBar1.Location = new System.Drawing.Point(17, 243);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.progressBar1.RightToLeftLayout = true;
+            this.progressBar1.Size = new System.Drawing.Size(152, 23);
+            this.progressBar1.TabIndex = 51;
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(94, 167);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(75, 23);
+            this.btnParar.TabIndex = 50;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(13, 167);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 49;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Mes:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Semana:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Dia:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "maiz_1.jpg");
+            this.imageList1.Images.SetKeyName(1, "maiz_2.jpg");
+            this.imageList1.Images.SetKeyName(2, "maiz_3.jpg");
+            this.imageList1.Images.SetKeyName(3, "maiz_4.jpg");
+            this.imageList1.Images.SetKeyName(4, "maiz_5.jpg");
+            this.imageList1.Images.SetKeyName(5, "maiz_6.jpg");
+            this.imageList1.Images.SetKeyName(6, "maiz_cosecha tierna.jpg");
+            this.imageList1.Images.SetKeyName(7, "maiz_maduro.jpg");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "luna.png");
+            this.imageList2.Images.SetKeyName(1, "sol.png");
+            // 
+            // pictureBoxVentilador
+            // 
+            this.pictureBoxVentilador.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxVentilador.Image = global::Maiz.Properties.Resources.Aspersor;
+            this.pictureBoxVentilador.Location = new System.Drawing.Point(649, 332);
+            this.pictureBoxVentilador.Name = "pictureBoxVentilador";
+            this.pictureBoxVentilador.Size = new System.Drawing.Size(46, 38);
+            this.pictureBoxVentilador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxVentilador.TabIndex = 43;
+            this.pictureBoxVentilador.TabStop = false;
+            // 
+            // pictureBoxSolLuna
+            // 
+            this.pictureBoxSolLuna.Image = global::Maiz.Properties.Resources.Sol;
+            this.pictureBoxSolLuna.Location = new System.Drawing.Point(237, 36);
+            this.pictureBoxSolLuna.Name = "pictureBoxSolLuna";
+            this.pictureBoxSolLuna.Size = new System.Drawing.Size(125, 106);
+            this.pictureBoxSolLuna.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxSolLuna.TabIndex = 41;
+            this.pictureBoxSolLuna.TabStop = false;
+            // 
+            // pictureBoxCultivo
+            // 
+            this.pictureBoxCultivo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCultivo.Image = global::Maiz.Properties.Resources.maiz_1;
+            this.pictureBoxCultivo.Location = new System.Drawing.Point(567, 120);
+            this.pictureBoxCultivo.Name = "pictureBoxCultivo";
+            this.pictureBoxCultivo.Size = new System.Drawing.Size(51, 250);
+            this.pictureBoxCultivo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxCultivo.TabIndex = 42;
+            this.pictureBoxCultivo.TabStop = false;
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInterval.Location = new System.Drawing.Point(69, 42);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(100, 20);
+            this.txtInterval.TabIndex = 52;
+            this.txtInterval.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInterval_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Intervalo:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 54;
+            this.button1.Text = "Reiniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 55;
+            this.button2.Text = "Llenar Tanque";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 25);
+            this.label6.TabIndex = 56;
+            this.label6.Text = "Agua al ???%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnEmpezar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxVentilador);
+            this.Controls.Add(this.pictureBoxSolLuna);
+            this.Controls.Add(this.pictureBoxCultivo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVentilador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSolLuna)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCultivo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxVentilador;
+        private System.Windows.Forms.PictureBox pictureBoxSolLuna;
+        private System.Windows.Forms.PictureBox pictureBoxCultivo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Maiz/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        int i, dias, semanas, meses, cambio,tanque;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            dias = 0;
+            semanas = 0;
+            meses = 0;
+            cambio = 0;
+            tanque = 0;
+            progressBar1.Value = tanque;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tanque = 100;
+            progressBar1.Value=(tanque);
+            label6.Text = "Agua al 100%";
+        }
+
+        private void txtInterval_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32&&e.KeyChar<=47)||(e.KeyChar>=58&&e.KeyChar<=255 ) )
+            {
+                if ( e.KeyChar != 127)
+                {
+                    MessageBox.Show("no es un numero!!!");
+                e.Handled = true;
+                return;
+                }
+               
+            }
+        
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnEmpezar_Click(object sender, EventArgs e)
+        {
+            if (txtInterval.Text == "")
+            {
+                MessageBox.Show("vacio");
+            }
+            else if (tanque <= 0) { MessageBox.Show("tanque vacio"); }
+            else {
+                timer1.Start();
+                timer1.Interval = Convert.ToInt32(txtInterval.Text);
+            }
+            
+        }
+        int aux;
+        private void timer1_Tick(object sender, EventArgs e)     
+
+        {
+            label6.Text = "Agua al " + progressBar1.Value + "%";
+            if (progressBar1.Value <= 0)
+            {
+                aux++;
+                if (aux==1)
+                {
+ MessageBox.Show("tanque Vacio!!!");
+                    aux = 0;
+                }
+               
+                timer1.Stop();
+            }
+            else
+            {
+                progressBar1.Value = tanque;
+            }
+            i++;
+                label1.Text = i.ToString();
+                if (i % 1 == 0)
+                {
+                    if (label3.Text == "dia")
+                    {
+                        label3.Text = "noche";
+                        pictureBoxSolLuna.Image = imageList2.Images[0];
+                        dias++;
+                        label4.Text = "Dia: " + dias;
+                    }
+                    else
+                    {
+                        pictureBoxSolLuna.Image = imageList2.Images[1];
+                        label3.Text = "dia";
+                    }
+
+                }
+                int cond_sem = (7) * (semanas + 1);
+                if (dias % 7 == 0 && dias >= cond_sem)
+                {
+                    semanas++;
+                    label5.Text = "Semanas: " + semanas;
+                }
+                if (semanas % 2 != 0)
+                {
+                    pictureBoxVentilador.Visible = true;
+                    tanque = tanque - 5;
+
+                     
+                 }
+                else
+                { 
+                    pictureBoxVentilador.Visible = false; 
+                }  
+               
+                int cond_mes = (4) * (meses + 1);
+                if (semanas % 4 == 0 && semanas >= cond_mes)
+                {
+                    meses++;
+                    label7.Text = "Mes: " + meses;
+                }
+                int cond_cam = 2 * (cambio + 1);
+                if (semanas % 2 == 0 && semanas >= cond_cam)
+                {
+
+                    pictureBoxCultivo.Image = imageList1.Images[cambio];
+                    if (cambio == 7)
+                    {
+                        cambio = 0;
+                        timer1.Enabled = false;
+                        MessageBox.Show("a cosechar");
+                    }
+                    cambio++;
+                }
+          
+            }
+       
+
+    }
+}
